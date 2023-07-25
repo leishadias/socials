0f8012a553dc276e72736e2c46bfbd1ad1c8d9d7
Leisha Dias <leishadias18@gmail.com>
2023-07-25 23:17:48 +0530

    import and set ejs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const port = 8000;
 const app = express();
-
+app.set('view engine', 'ejs');
+app.set('views', './views')
 app.listen(port, function(err){
     if (err){
         console.log(`Error: ${err}`);
